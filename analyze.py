c888a5bf6b91e775a35a8c0b6a02b1013846fe89
johnnyle24 <johnny.le24@gmail.com>
2017-11-20 00:09:50 -0700

    Feature creation

---
+++ analyze.py
@@ -0,0 +1,47 @@
+# Converts the csv file into a feature set
+
+import csv
+
+
+def trungle():
+	with open("Sample Data/84070-UT.csv", "rb") as source, open("result", "wb") as result:
+		rdr = csv.reader(source)
+		wtr = csv.writer(result)
+
+		csv_headings = next(rdr)
+
+		examples = []
+		index = 0
+		headers = []
+
+		for header in csv_headings:
+			headers.append(header)
+
+
+		for r in rdr:
+			index = 0
+			example = []
+			for c in r:
+				if headers[index] == 'Year':
+					year = int(c) - 1990 # Base Year
+					year = year * 12
+					month = int(r[index + 1])
+
+					# combine year and month
+
+					combine = year + month
+
+					example.append(combine)
+					print(combine)
+				else:
+					example.append(c)
+				index += 1
+
+			examples.append(example)
+
+
+
+
+
+
+
