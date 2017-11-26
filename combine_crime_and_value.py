eb134f39af220f5befce1dbef21c9799a395009c
johnnyle24 <johnny.le24@gmail.com>
2017-11-26 15:04:40 -0700

    Combining Crime and Values

--- combine_crime_and_value.py
+++ combine_crime_and_value.py
@@ -1,17 +1,23 @@
 import csv
+import os, os.path
+import re
 import pdb
 from datetime import datetime
 from dateutil import parser
 import re
 
+def add_data(row, data_dict, data_list, name):
+    for element in data_list:
+        if element in data_dict[name]:
+            row.append(data_dict[name][element])
+        else:
+            row.append(0)
+
 zips = {}
 
-with open('data/Crime_Data_from_2010_to_Present.csv') as f:
+with open('Sample Data/crime_data_total.csv') as f:
     reader = list(csv.DictReader(f))
 
-
-
-    num_lines = len(reader)
     i = 0
     for row in reader:
         if row['Zip Code'] in zips:
@@ -19,6 +25,52 @@ with open('data/Crime_Data_from_2010_to_Present.csv') as f:
         else:
             zips[row['Zip Code']] = {
                 row['MonthId']: {
-                'Crimes': row['Crimes']
-                }
+                    'Crimes': row['Crimes'],
+                    'MedianSquareValue': 0
+                },
             }
+
+regex = '^[0-9]*-[A-Z][A-Z]$'
+
+DIR = '/Users/johnnyle/Machine Learning/Real-Estate-Prediction/Sample Data'
+directory_length = len([name for name in os.listdir(DIR) if os.path.isfile(os.path.join(DIR, name))])
+
+for name in os.listdir(DIR):
+    if os.path.isfile(os.path.join(DIR, name)):
+        if re.match(regex, name):
+            zip = name[:4]
+
+            with open('data/' + name + '.csv') as g:
+                valueReader = list(csv.DictReader(g))
+
+                i = 0
+                for row in valueReader:
+                    region_zip = row['Region'][:4]
+                    if region_zip in zips:
+                        zips[region_zip][row['MonthId']]['MedianSquareValue'] = row['Zip_MedianValuePerSqft_AllHomes']
+                    else:
+                        zips[row['Zip Code']] = {
+                            row['MonthId']: {
+                                'Crimes': 0,
+                                'MedianSquareValue': row['Zip_MedianValuePerSqft_AllHomes']
+                            },
+                        }
+
+with open('Sample Data/crime_and_value.csv', 'w') as f:
+    writer = csv.writer(f)
+
+    tc = ['total']
+
+    headers = ['Zip Code', 'MonthId', 'Total', 'MedianSquareValue']
+    rows = [headers]
+
+    for zip_code in zips:
+        for monthId in zips[zip_code]:
+            row = [zip_code, monthId]
+            data_dict = zips[zip_code][monthId]
+            add_data(row, data_dict, tc, 'Crimes')
+            rows.append(row)
+    writer.writerows(rows)
+
+
+# with open('Sample Data/ ')
