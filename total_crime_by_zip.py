97e9ffc89e1f52111d6b9f63a3e30731f679957f
tannerb <tannerb@lab1-11.eng.utah.edu>
2017-12-05 11:32:10 -0700

    converting data. haven't run on full set yet

--- total_crime_by_zip.py
+++ total_crime_by_zip.py
@@ -6,6 +6,7 @@ from datetime import datetime
 from dateutil import parser
 import time
 import re
+import math
 
 # geolocator = Nominatim()
 #
@@ -21,37 +22,28 @@ import re
 
 zip_code_data = {}
 all_crime_codes = set()
+all_victim_ages = set()
+all_victim_sexes = set()
+all_victim_descents = set()
 
-coords = {}
+zips = {}
+
+california = 90000
 
 with open('Sample Data/zipcodes_and_coords.csv') as f:
-    reader = list(csv.DictReader(f))
-    num_lines = len(reader)
+    reader = csv.DictReader(f)
 
     for r in reader:
 
-        current_zip = r['ZIP']
+        current_zip = int(r['ZIP'])
+        if current_zip < california:
+            continue
 
-        latitude = r['LAT']
-        latitude = str(round(float(latitude), 2))
+        latitude = float(r['LAT'])
 
-        longitude = r['LNG']
-        longitude = str(round(float(longitude), 2))
+        longitude = float(r['LNG'])
+        zips[current_zip] = [latitude,longitude]
 
-        if latitude in coords:
-            coords[latitude][longitude] = {
-                'ZIP': current_zip
-            }
-        else:
-            coords[latitude] = {
-                longitude: {
-                    'ZIP': current_zip
-                }
-            }
-
-        print(latitude + ", " + longitude)
-
-        print(coords[latitude][longitude]['ZIP'])
 
 
 def increment_or_create(data_dict, key):
@@ -70,13 +62,25 @@ def get_col_value(row, name):
     return '{}={}'.format(name, val)
 
 
+def euclidean(lat1,lon1,lat2,lon2):
+    return math.sqrt((lat2 - lat1)**2+(lon2 - lon1)**2)
 
+
+def coords_to_zip(lat,lon):
+    smallest_dist = float('inf')
+    closest_zip = None
+    for zip in zips:
+        coords = zips[zip]
+        dist = euclidean(coords[0],coords[1],lat,lon)
+        if dist < smallest_dist:
+            smallest_dist = dist
+            closest_zip = zip
+    return closest_zip
 
 zip_p = re.compile('(9[0-9][0-9][0-9][0-9])')
 
 with open('data/Crime_Data_from_2010_to_Present.csv') as f:
-    reader = list(csv.DictReader(f))
-    num_lines = len(reader)
+    reader = csv.DictReader(f)
     i = 0
     for row in reader:
         if 'Location ' in row:
@@ -88,47 +92,51 @@ with open('data/Crime_Data_from_2010_to_Present.csv') as f:
             if location_s[1][1:]=='':
                 continue
 
-            lat = location_s[0][1:]
-            lon = location_s[1][0:-1]
-
-            lat = str(round(float(lat),2))
-
-            lon = str(round(float(lon),2))
-
-            if lat in coords:
-                if lon in coords[lat]:
-                    zip_code = coords[lat][lon]['ZIP']
-                else:
-                    while lon not in coords[lat]:
-                        lon = str(0.01 + float(lon))
-                    zip_code = coords[lat][lon]['ZIP']
-            else:
-                while lat not in coords:
-                    lat = str(0.01 + float(lat))
-                if lon in coords[lat]:
-                    zip_code = coords[lat][lon]['ZIP']
-                else:
-                    while lon not in coords[lat]:
-                        lon = str(0.01 + float(lon))
-                    zip_code = coords[lat][lon]['ZIP']
-
-                
+            lat = float(location_s[0][1:])
+            lon = float(location_s[1][0:-1])
+            zip_code = coords_to_zip(lat,lon)
             
 
             dt = parser.parse(row['Date Occurred'])
 
             monthId = dt.year * 12 + dt.month
 
+            crime_code = get_col_value(row,'Crime Code')
+            victim_age = get_col_value(row,'Victim Age')
+            victim_sex = get_col_value(row,'Victim Sex')
+            victim_descent = get_col_value(row,'Victim Descent')
+
+            all_crime_codes.add(crime_code)
+            all_victim_ages.add(victim_age)
+            all_victim_sexes.add(victim_sex)
+            all_victim_descents.add(victim_descent)
+
             if zip_code in zip_code_data:
                 if monthId in zip_code_data[zip_code]:
                     try:
                         increment_or_create(zip_code_data[zip_code][monthId]['total_crime'], 'total')
+                        increment_or_create(zip_code_data[zip_code][monthId]['crime_codes'],crime_code)
+                        increment_or_create(zip_code_data[zip_code][monthId]['victim_ages'],victim_age)
+                        increment_or_create(zip_code_data[zip_code][monthId]['victim_sexes'],victim_sex)
+                        increment_or_create(zip_code_data[zip_code][monthId]['victim_descents'],victim_descent)
                     except KeyError:
                         pdb.set_trace()
                 else:
                     zip_code_data[zip_code][monthId] = {
                         'total_crime': {
                             'total': 1
+                        },
+                        'crime_codes' : {
+                            crime_code : 1
+                        },
+                        'victim_ages' : {
+                            victim_age : 1
+                        },
+                        'victim_sexes' : {
+                            victim_sex : 1
+                        },
+                        'victim_descents' : {
+                            victim_descent : 1
                         }
                     }
             else:
@@ -136,11 +144,26 @@ with open('data/Crime_Data_from_2010_to_Present.csv') as f:
                     monthId: {
                         'total_crime': {
                             'total': 1
+                        },
+                        'crime_codes' : {
+                            crime_code : 1
+                        },
+                        'victim_ages' : {
+                            victim_age : 1
+                        },
+                        'victim_sexes' : {
+                            victim_sex : 1
+                        },
+                        'victim_descents' : {
+                            victim_descent : 1
                         }
                     }
                 }
             i += 1
-            print('{} out of {}'.format(i, num_lines))
+            print(i)
+            if i > 1000:
+                break
+            
 
 
 def add_data(row, data_dict, data_list, name):
@@ -154,9 +177,14 @@ def add_data(row, data_dict, data_list, name):
 with open('Sample Data/crime_data_total-2.csv', 'w') as f:
     writer = csv.writer(f)
 
+    cc = list(all_crime_codes)
+    va = list(all_victim_ages)
+    vs = list(all_victim_sexes)
+    vd = list(all_victim_descents)
+
     tc = ['total']
 
-    headers = ['Zip Code', 'MonthId'] + tc
+    headers = ['Zip Code', 'MonthId'] + tc + cc + va + vs + vd
     rows = [headers]
 
     for zip_code in zip_code_data:
@@ -168,6 +196,11 @@ with open('Sample Data/crime_data_total-2.csv', 'w') as f:
                 row.append(data_dict['total_crime']['total'])
             else:
                 row.append(0)
+
+            add_data(row,data_dict,cc,'crime_codes')
+            add_data(row,data_dict,va,'victim_ages')
+            add_data(row,data_dict,vs,'victim_sexes')
+            add_data(row,data_dict,vd,'victim_descents')
 
 
             rows.append(row)
