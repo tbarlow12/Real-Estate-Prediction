dc630777d34d6a86f1c8f5860336d6cc259a2c95
johnnyle24 <johnny.le24@gmail.com>
2017-12-10 15:52:07 -0700

    Ridge

---
+++ models/ridge_lasso_regression.py
@@ -0,0 +1,17 @@
+# Ridge and Lasso Regression
+
+import numpy as np
+
+def ridge_regression(x_train, y_train, lam):
+    
+    X = np.array(x_train)
+    ones = np.ones(len(X))
+    X = np.column_stack((ones,X))
+    y = np.array(y_train)
+    
+    Xt = np.transpose(X)
+    lambda_identity = lam*np.identity(len(Xt))
+    theInverse = np.linalg.inv(np.dot(Xt, X)+lambda_identity)
+    w = np.dot(np.dot(theInverse, Xt), y)
+    return w, lambda x: dot(w,x)
+
